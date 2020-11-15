5821800cbc126e88c2b5ae481d326c1833ff09f3
Shane O'Moore <shaneiadt@gmail.com>
2020-11-15 12:13:32 +0000

    js switch css theme of root document element

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('JS READY');
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+function switchTheme({ target: { checked } }) {
+    document.documentElement.setAttribute('data-theme', checked ? 'dark' : 'light');
+}
+
+toggleSwitch?.addEventListener("change", switchTheme);
