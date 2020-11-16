1fae36004b7601d74a336c489620a7c086228682
Shane O'Moore <shaneiadt@gmail.com>
2020-11-16 16:15:42 +0000

    lightMode function

--- script.js
+++ script.js
@@ -18,7 +18,14 @@ function darkMode() {
 }
 
 function lightMode() {
-
+    nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    toggleIcon.children[0].textContent = "Light Mode";
+    toggleIcon.children[1].classList.remove("fa-moon");
+    toggleIcon.children[1].classList.add("fa-sun");
+    image1.src = "img/undraw_proud_coder_light.svg";
+    image2.src = "img/undraw_feeling_proud_light.svg";
+    image3.src = "img/undraw_conceptual_idea_light.svg";
 }
 
 function switchTheme({ target: { checked } }) {
