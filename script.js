25b975c7e77c2722b21b52f48479eb2f1022d5ca
Shane O'Moore <shaneiadt@gmail.com>
2020-11-16 16:14:35 +0000

    darkMode function

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.querySelector('#nav');
+const toggleIcon = document.querySelector('#toggle-icon');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2');
+const image3 = document.querySelector('#image3');
+const textBox = document.querySelector('#text-box');
+
+function darkMode() {
+    nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    toggleIcon.children[0].textContent = "Dark Mode";
+    toggleIcon.children[1].classList.remove("fa-sun");
+    toggleIcon.children[1].classList.add("fa-moon");
+    image1.src = "img/undraw_proud_coder_dark.svg";
+    image2.src = "img/undraw_feeling_proud_dark.svg";
+    image3.src = "img/undraw_conceptual_idea_dark.svg";
+}
+
+function lightMode() {
+
+}
 
 function switchTheme({ target: { checked } }) {
+    if (checked) {
+        darkMode();
+    } else {
+        lightMode();
+    }
+
     document.documentElement.setAttribute('data-theme', checked ? 'dark' : 'light');
 }
 
