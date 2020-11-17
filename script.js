3c29fc79c7c61391d1e845afa4a52897b65f9858
Shane O'Moore <shaneiadt@gmail.com>
2020-11-17 08:58:09 +0000

    use localStorage to save the current theme used

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ const image1 = document.querySelector('#image1');
 const image2 = document.querySelector('#image2');
 const image3 = document.querySelector('#image3');
 const textBox = document.querySelector('#text-box');
+const currentTheme = localStorage.getItem('data-theme');
+
+function setTheme(theme) {
+    localStorage.setItem('data-theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    nav.style.backgroundColor = theme === 'dark' ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+    textBox.style.backgroundColor = theme === 'dark' ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+    toggleIcon.children[0].textContent = theme === 'dark' ? "Dark Mode" : "Light Mode";
+    toggleIcon.children[1].classList.remove(theme === 'dark' ? "fa-sun" : "fa-moon");
+    toggleIcon.children[1].classList.add(theme === 'dark' ? "fa-moon" : "fa-sun");
+    image1.src = `img/undraw_proud_coder_${theme}.svg`;
+    image2.src = `img/undraw_feeling_proud_${theme}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
+}
 
 function switchTheme({ target: { checked } }) {
-    document.documentElement.setAttribute('data-theme', checked ? 'dark' : 'light');
-    nav.style.backgroundColor = checked ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
-    textBox.style.backgroundColor = checked ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
-    toggleIcon.children[0].textContent = checked ? "Dark Mode" : "Light Mode";
-    toggleIcon.children[1].classList.remove(checked ? "fa-sun" : "fa-moon");
-    toggleIcon.children[1].classList.add(checked ? "fa-moon" : "fa-sun");
-    image1.src = `img/undraw_proud_coder_${checked ? 'dark' : 'light'}.svg`;
-    image2.src = `img/undraw_feeling_proud_${checked ? 'dark' : 'light'}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${checked ? 'dark' : 'light'}.svg`;
+    setTheme(checked ? 'dark' : 'light');
 }
 
 toggleSwitch?.addEventListener("change", switchTheme);
+
+if (currentTheme) {
+    setTheme(currentTheme);
+    toggleSwitch.checked = currentTheme === 'dark' ? true : false;
+}
